056a570f4ad754c228fd62ae37dcfe6b87213bbb
3LieLs <3lielfernandesribeiro@gmail.com>
2023-08-26 18:24:53 -0300

    Atualização

---
+++ assets/js/Menu_Script.js
@@ -0,0 +1,104 @@
+/*INTRODUÇÃO------------INTRODUÇÃO---------------INTRODUÇÃO---------------INTRODUÇÃO---------------INTRODUÇÃO---------------INTRODUÇÃO*/
+var introducaoAtalhoVal = window.document.querySelector('a#introducaoAtalho')
+introducaoAtalhoVal.addEventListener('mouseover', animacaoAtalhoHover)
+introducaoAtalhoVal.addEventListener('mouseout', animacaoAtalhoOut)
+
+function animacaoAtalhoHover() {
+    introducaoAtalhoVal.style.animation = 'atalhoAnimacaoHover 0.7s 0ms forwards'
+}
+function animacaoAtalhoOut() {
+    introducaoAtalhoVal.style.animation = 'atalhoAnimacaoOut 0.7s 0ms forwards'
+}
+/*------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*FUNDAMENTAÇÃO---------FUNDAMENTAÇÃO---------FUNDAMENTAÇÃO------FUNDAMENTAÇÃO---FUNDAMENTAÇÃO-----FUNDAMENTAÇÃO------------FUNDAMENTAÇÃO*/
+var fundamentacaoAtalhoVal = window.document.querySelector('a#fundamentacaoAtalho')
+fundamentacaoAtalhoVal.addEventListener('mouseover', fundamentacaoAtalhoHover)
+fundamentacaoAtalhoVal.addEventListener('mouseout', fundamentacaoAtalhoOut)
+
+function fundamentacaoAtalhoHover() {
+    fundamentacaoAtalhoVal.style.animation = 'atalhoAnimacaoHover 0.7s 0ms forwards'
+}
+function fundamentacaoAtalhoOut() {
+    fundamentacaoAtalhoVal.style.animation = 'atalhoAnimacaoOut 0.7s 0ms forwards'
+}
+/*------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*OBJETIVOS-------------OBJETIVOS----------------OBJETIVOS----------------OBJETIVOS----------------OBJETIVOS----------------OBJETIVOS*/
+var objetivosAtalhoVal = window.document.querySelector('a#objetivosAtalho')
+objetivosAtalhoVal.addEventListener('mouseover', objetivosAtalhoHover)
+objetivosAtalhoVal.addEventListener('mouseout', objetivosAtalhoOut)
+
+function objetivosAtalhoHover() {
+    objetivosAtalhoVal.style.animation = 'atalhoAnimacaoHover 0.7s 0ms forwards'
+}
+function objetivosAtalhoOut() {
+    objetivosAtalhoVal.style.animation = 'atalhoAnimacaoOut 0.7s 0ms forwards'
+}
+/*------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*REDESOCIAL------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL*/
+var discordVal = window.document.querySelector('img#discord')
+discordVal.addEventListener('mouseover', discordHover)
+discordVal.addEventListener('mouseout', discordOut)
+
+function discordHover() {
+    discordVal.style.animation = 'redeSocialAnimacaoHover 0.1s 0ms forwards'
+    discordVal.style.boxShadow = '3px 3px 10px 0px black'
+}
+function discordOut() {
+    discordVal.style.animation = 'redeSocialAnimacaoOut 0.1s 0ms forwards'
+    discordVal.style.boxShadow = 'none'
+}
+/*------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*REDESOCIAL------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL*/
+var instagramVal = window.document.querySelector('img#instagram')
+instagramVal.addEventListener('mouseover', instagramHover)
+instagramVal.addEventListener('mouseout', instagramOut)
+
+function instagramHover() {
+    instagramVal.style.animation = 'redeSocialAnimacaoHover 0.1s 0ms forwards'
+    instagramVal.style.boxShadow = '3px 3px 10px 0px black'
+}
+function instagramOut() {
+    instagramVal.style.animation = 'redeSocialAnimacaoOut 0.1s 0ms forwards'
+    instagramVal.style.boxShadow = 'none'
+}
+/*------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*REDESOCIAL------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL*/
+var youtubeVal = window.document.querySelector('img#youtube')
+youtubeVal.addEventListener('mouseover', youtubeHover)
+youtubeVal.addEventListener('mouseout', youtubeOut)
+
+function youtubeHover() {
+    youtubeVal.style.animation = 'redeSocialAnimacaoHover 0.1s 0ms forwards'
+    youtubeVal.style.boxShadow = '3px 3px 10px 0px black'
+}
+function youtubeOut() {
+    youtubeVal.style.animation = 'redeSocialAnimacaoOut 0.1s 0ms forwards'
+    youtubeVal.style.boxShadow = 'none'
+}
+/*------------------------------------------------------------------------------------------------------------------------------------*/
+
+/*REDESOCIAL------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL---------------REDESOCIAL*/
+var gmailVal = window.document.querySelector('img#gmail')
+gmailVal.addEventListener('mouseover', gmailHover)
+gmailVal.addEventListener('mouseout', gmailOut)
+
+function gmailHover() {
+    gmailVal.style.animation = 'redeSocialAnimacaoHover 0.1s 0ms forwards'
+    gmailVal.style.boxShadow = '3px 3px 10px 0px black'
+}
+function gmailOut() {
+    gmailVal.style.animation = 'redeSocialAnimacaoOut 0.1s 0ms forwards'
+    gmailVal.style.boxShadow = 'none'
+}
+/*------------------------------------------------------------------------------------------------------------------------------------*/
