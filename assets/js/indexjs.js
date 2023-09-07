dbb3b14689b4948dd87ba6fe16345ac45b9d797a
3LieLs <3lielfernandesribeiro@gmail.com>
2023-09-07 18:15:27 -0300

    Atualização

--- assets/js/indexjs.js
+++ assets/js/indexjs.js
@@ -1,13 +1,27 @@
-const observer = new IntersectionObserver(entries => {
+const observerEsquerda = new IntersectionObserver(entries => {
     Array.from(entries).forEach(entry => {
         if (entry.intersectionRatio >= .4) {
-            entry.target.classList.add('iniciarVisivel')
+            entry.target.classList.add('iniciarVisivelEsquerda')
         }
     })
 },{
     threshold: [0, .5, 1]
 })
 
-Array.from(document.querySelectorAll('.iniciarEscondido')).forEach(element => {
-    observer.observe(element)
+const observerDireita = new IntersectionObserver(entries => {
+    Array.from(entries).forEach(entry => {
+        if (entry.intersectionRatio >= .4) {
+            entry.target.classList.add('iniciarVisivelDireita')
+        }
+    })
+},{
+    threshold: [0, .5, 1]
+})
+
+Array.from(document.querySelectorAll('.iniciarEscondidoEsquerda')).forEach(element => {
+    observerEsquerda.observe(element)
+})
+
+Array.from(document.querySelectorAll('.iniciarEscondidoDireita')).forEach(element => {
+    observerDireita.observe(element)
 })
